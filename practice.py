22b90b8d5b2b47efbb724fe721860bb64a1156ba
Younghoon <gcloud1025@gmail.com>
2022-01-27 20:34:33 +0900

    0127

--- practice.py
+++ practice.py
@@ -1,3 +1,12 @@
-lst = [1, 5, 4, 2, 3]
-sorted(lst)
-print(lst)
+n = int(input())
+for i in range(n):
+    lst = input()
+    total = 0
+    basic_score = 1
+    for char in lst:
+        if char == 'O':
+            total += basic_score
+            basic_score += 1
+        if char == 'X':
+            basic_score = 1
+    print(total)
