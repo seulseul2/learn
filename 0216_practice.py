ea5475b9fca5d2802208bece96abcf74b8f70a77
Younghoon <gcloud1025@gmail.com>
2022-02-17 09:28:14 +0900

    0217

--- 0216_practice.py
+++ 0216_practice.py
@@ -1,24 +1,19 @@
-t = 'This is a book~!'
-p = 'is'
-
-# patter이 text에 처음 나타난 위치를 return 합니다.
-# pattern이 없으면 -1을 return
-def Bru(pattern, text):
-    N = len(text)
-    M = len(pattern)
-    i = j = 0
-
-    while i<N and j<M:
-        if text[i] != pattern[j]:
-            i = i - j + 1
-            #i = i + 1
-            j = 0
-        else:
-            i += 1
-            j += 1
-    if j==M:
-        return i-j
+def BruteForce(p, t):
+    i = 0 # t의 검색 인덱스
+    j = 0 # p의 검색 인덱스
+    while i < len(t) and j < len(p):
+        if t[i] != p[j]:
+            i = i-j
+            j = -1
+        i += 1
+        j += 1
+    if j == len(p):
+        return i - len(p)
     else:
         return -1
 
-print(Bru(p, t))
+T = int(input())
+for i in range(T):
+    p = input()
+    t = input()
+    print(BruteForce(p, t))
